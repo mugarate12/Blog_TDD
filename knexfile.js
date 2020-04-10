b28a8a1fd8bcc606b44a8657d1bf4d34cf079dae
unknown <serjumano17@gmail.com>
2020-04-10 06:39:48 -0300

    first commit

---
+++ knexfile.js
@@ -0,0 +1,46 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'mysql',
+    connection: {
+      database: 'blog_knex',
+      user:     'root',
+      password: 'majuge123'
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
---
+++ src/routes.js
@@ -0,0 +1,7 @@
+const { Router } = require('express')
+
+const routes = Router()
+
+// declaro minhas rotas
+
+module.exports = routes
